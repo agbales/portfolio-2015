af442bf06a5aa9ee92831d5f76f960c7d66571ba
agbales <andrewbales@Andrews-MacBook-Pro-2.local>
2015-12-15 16:11:13 -0500

    Javascript Update
    Video button update.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
-function toggle_visibility(id) {
-   	var e = document.getElementById(id);
-   	if(e.style.display == 'block')
-    	e.style.display = 'none';
-   	else
-    	e.style.display = 'block';
+function toggleVisibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'block')
+    e.style.display = 'none';
+  else
+    e.style.display = 'block';
+}
+
+function videoText(id) {
+    var element = document.getElementById(id);
+    if(element.innerHTML === 'Video') 
+      element.innerHTML = 'Close!';
+    else
+       element.innerHTML = 'Video';
 }
